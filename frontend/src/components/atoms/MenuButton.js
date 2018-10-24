1cbf16c044b419eff698ce398569e21b9722a727
Veronika Hnilickova <hniv00@vse.cz>
2018-10-24 19:32:37 +0200

    New menu button

--- frontend/src/components/atoms/MenuButton.js
+++ frontend/src/components/atoms/MenuButton.js
@@ -2,9 +2,20 @@ import React, { Component } from "react";
 import '../../App.css';
 
 export class MenuButton extends Component {
+  myFunction(x) {
+    x.classList.toggle("change");
+}
+
   render() {
     return (
-      <button id="roundButton" onMouseDown={this.props.handleMouseDown}></button>
+      <div>
+        <div class="buttonContainer" onclick="myFunction(this)"
+             onMouseDown={this.props.handleMouseDown}>
+          <div class="bar1"></div>
+          <div class="bar2"></div>
+          <div class="bar3"></div>
+        </div>
+      </div>
     );
   }
 }
