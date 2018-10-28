cd6b071d28e51614cb65304820df278c16c94871
Daniel Vrána <vranadaniel@gmail.com>
2018-10-28 12:20:46 +0100

    cleaned landing page code

--- frontend/src/components/organisms/ExaminationList.js
+++ frontend/src/components/organisms/ExaminationList.js
@@ -6,9 +6,9 @@ import {Examination} from '../molecules/Examination';
 export class ExaminationList extends Component {
   render() {
     return (
-    <div>
+    <div style={{ margin: '2%' }}>
         <h5>Seznam vyhledaných preventivních vyšetření</h5>
-        <Examination></Examination>
+        <Examination/>
       <div style={{ height: '20px' }} />
         <Button color="info">Chci vidět více výsledků</Button>{' '}
     </div>
