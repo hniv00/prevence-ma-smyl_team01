ccf7012a30cedc9fcf2bfe693cfecf480b9dbe10
Petr Dvorský <petr.dvorsky9@gmail.com>
2018-11-02 10:15:28 +0100

    tooltip-experimental

--- frontend/src/components/pages/AdminLandingPage.js
+++ frontend/src/components/pages/AdminLandingPage.js
@@ -20,6 +20,7 @@ import {Heading} from '../atoms/Heading';
 import {Footer} from '../organisms/Footer';
 import {LogoutButton} from '../atoms/LogoutButton';
 import {MultiSelect} from '../atoms/MultiSelect';
+import {TooltipItem} from '../atoms/TooltipItem';
 
 //import logo from '../../img/logo_prevence.png';
 
@@ -49,7 +50,7 @@ export class AdminLandingPage extends Component {
         <br/>
         <h3 className="AdminWelcH2">Můžete začít spravovat vyšetření nebo příběhy. Vyberte akci z menu.</h3>
         <div>
-          <Jumbotron></Jumbotron>
+          <TooltipItem></TooltipItem>
         </div>
       </div>
     );
