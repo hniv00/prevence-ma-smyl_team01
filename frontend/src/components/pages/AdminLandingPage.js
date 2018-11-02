a22555e7be49e3843dcf3e552e5773cdfef04ef3
Petr Dvorský <petr.dvorsky9@gmail.com>
2018-11-02 09:37:22 +0100

    adminHelloPage-menu added

--- frontend/src/components/pages/AdminLandingPage.js
+++ frontend/src/components/pages/AdminLandingPage.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react'
 import {Parallax, Background} from 'react-parallax';
-import {Button} from 'reactstrap';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Container,
+  Jumbotron} from 'reactstrap';
 
 import {TopNav} from '../molecules/TopNav';
 import {Heading} from '../atoms/Heading';
@@ -8,22 +21,37 @@ import {Footer} from '../organisms/Footer';
 import {LogoutButton} from '../atoms/LogoutButton';
 import {MultiSelect} from '../atoms/MultiSelect';
 
-import logo from '../../img/logo_prevence.png';
+//import logo from '../../img/logo_prevence.png';
 
 export class AdminLandingPage extends Component {
   render() {
     return (
       <div>
-        <h2>Vítejte v admin modulu</h2>
-        <h3>Zde budete mít brzy možnost spravovat obsah webu ...</h3>
-
-          <div>
-              <LogoutButton></LogoutButton>
-          </div>
-          <div>
-              <MultiSelect></MultiSelect>
-          </div>
+      <div>
+        <Navbar className="AdminNavBar" color="light" light expand="md">
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink className="AdminNavItem" href="/admin-prevence/">Prevence</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="AdminNavItem" href="/admin-pribehy/">Příběhy</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="AdminNavItem" href="/admin-prevencemasmysl/">#PrevenceMaSmysl</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="AdminNavLogout" href="/admin-login/">Odhlásit</NavLink>
+              </NavItem>
+            </Nav>
+        </Navbar>
+      </div>
+        <h2 className="AdminWelcH2">Vítejte v admin modulu</h2>
+        <br/>
+        <h3 className="AdminWelcH2">Můžete začít spravovat vyšetření nebo příběhy. Vyberte akci z menu.</h3>
+        <div>
+          <Jumbotron></Jumbotron>
         </div>
+      </div>
     );
   }
 }
