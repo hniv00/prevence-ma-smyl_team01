07a597326eac8fb9d5c22e88a1515f7f3abd7dff
Veronika Hnilickova <hniv00@vse.cz>
2018-11-16 01:05:04 +0100

    admin modul - multiselect update

--- frontend/src/components/atoms/MultiSelect.js
+++ frontend/src/components/atoms/MultiSelect.js
@@ -4,13 +4,13 @@ import Select from 'react-select';
 export class MultiSelect extends Component {
   render() {
     return (
-      <Select style={{'border-color': '#17a2b8', 'border-radius': '20px', 'font-size': '10px'}}
+      <Select
         value={this.props.selectedOption}
         onChange={this.props.handleChange}
         options={this.props.options}
         isMulti={true}
         isSearchable={true}
-        placeholder="Nemoci v rodině"
+        placeholder="Vyberte..."
       />
     );
   }
--- frontend/src/components/molecules/ExamDiagnosis.js
+++ frontend/src/components/molecules/ExamDiagnosis.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import { Label } from 'reactstrap';
 
 
-import {MultiSelectAna} from '../atoms/MultiSelectAna';
+import {MultiSelectDiag} from '../atoms/MultiSelectDiag';
 
 export class ExamDiagnosis extends Component {
   render() {
     return (
       <div>
         <Label for="exampleText">Diagnózy</Label>
-        <MultiSelectAna/>
+        <MultiSelectDiag/>
       </div>
     );
   }
