c101bfb5554b66abf009519112890b68de87f7db
Veronika Hnilickova <hniv00@vse.cz>
2018-11-27 16:16:29 +0100

    redux for admin-diagnosis name and desrc but not working

--- frontend/src/components/molecules/DiagDescription.js
+++ frontend/src/components/molecules/DiagDescription.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react"
 
 import {  FormGroup, Form, Label, Input } from 'reactstrap';
+import { connect } from 'react-redux';
+import { setDescription } from '../../services/AdminDiagnosis/actions';
 
-export class DiagDescription extends Component {
+export class DiagDescriptionRaw extends Component {
   render() {
+    const { inputDescr } = this.props;
     return (
       <Form>
         <FormGroup>
           <Label for="exampleText">Popis diagnózy</Label>
-          <Input type="textarea" name="text" id="exampleText" />
+          <Input type="textarea" name="text" id="exampleText" value={inputDescr}/>
         </FormGroup>
       </Form>
     );
   }
 }
+
+
+const mapStateToProps = state => ({
+  inputDescr: state.createDiagnosis.description
+});
+
+const mapDispatchToProps = {
+  setDescription
+};
+
+export const DiagDescription = connect(mapStateToProps, mapDispatchToProps)(DiagDescriptionRaw);
