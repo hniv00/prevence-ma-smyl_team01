229fcdb2efcfa3ca19fc43125069605ca1b51e15
Veronika Hnilickova <hniv00@vse.cz>
2018-11-27 15:21:07 +0100

    redux for admin-diagnosis selected examinations

--- frontend/src/store/createRootReducer.js
+++ frontend/src/store/createRootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import filtrationReducer from '../services/Filtration/reducer'
 import partnersReducer from '../services/Partners/reducer'
+import diagnosisReducer from '../services/AdminDiagnosis/reducer'
 
 export default combineReducers({
     filterState: filtrationReducer,
     filterPartner: partnersReducer,
+    createDiagnosis: diagnosisReducer,
 });
