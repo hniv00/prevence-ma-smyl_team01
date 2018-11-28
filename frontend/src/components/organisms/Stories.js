88ada7a5ade13a3fd048ff77046ef6dac0b19928
Veronika Hnilickova <hniv00@vse.cz>
2018-11-28 23:57:04 +0100

    admin examination repair

--- frontend/src/components/organisms/Stories.js
+++ frontend/src/components/organisms/Stories.js
@@ -13,25 +13,27 @@ const data = [{
 }, {
     id: 2,
     name: 'Daniela',
-    age: '23',
-    description: `Po procházce v lese jsem se pořádně neprohlédl.
-    O několik dní později jsem si na těle našel zakouslé klíště, okolo
-    místa vkusu zarudlé místo. Ihned jsem jel k lékaři, ale nepomohlo to.`
+    age: '40',
+    description: `Kamarádka mě přemluvila, abych s ní šla na Den prevence.
+    Jen díky tomu mi tehdy lékaři včas odhalili zhoubný nádor.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.`
 }, {
     id: 3,
     name: 'Jiří',
     age: '53',
-    description: `Po procházce v lese jsem se pořádně neprohlédl.
-    O několik dní později jsem si na těle našel zakouslé klíště, okolo
-    místa vkusu zarudlé místo. Ihned jsem jel k lékaři, ale nepomohlo to.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit.`
+    description: `Na preventivní prohlídky chodím pravidelně, když mě začaly trápit
+    problémy s častým močením, ihned jsem šel za svým urologem.
+    Udělal jsem dobře. I když jsem nakonec nebyl nijak vážně nemocný, člověk nikdy neví,
+    co se může stát a jaká choroba ho přemůže. Lepší je prevence.`
 }, {
     id: 4,
     name: 'Dominika',
     age: '63',
-    description: `Po procházce v lese jsem se pořádně neprohlédl.
-    O několik dní později jsem si na těle našel zakouslé klíště, okolo
-    místa vkusu zarudlé místo. Ihned jsem jel k lékaři, ale nepomohlo to.`
+    description: `Nahmatala jsem si bulku v podpaží a ihned jsem šla k lékaři.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.`
 }, {
     id: 5,
     name: 'Roman',
@@ -68,7 +70,7 @@ export class Stories extends Component {
         const settings = {
           dots: true,
           infinite: true,
-          speed: 500,
+          speed: 1500,
           autoplay: true,
           pauseOnFocus: true,
           arrows: true,
@@ -106,8 +108,7 @@ export class Stories extends Component {
     return (
       <div className='parallax-content' id="stories">
         <h4>Příběhy "Za 5min 12"</h4>
-        <h5>Příběhy těch, kterým prevence zachránila život</h5>
-        <p id="cssContent">Příběhy za 5 minut 12. TBD Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer pellentesque quam vel velit. Praesent vitae arcu tempor neque lacinia pretium. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Aliquam in lorem sit amet leo accumsan lacinia. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nunc tincidunt ante vitae massa. Nam quis nulla. Phasellus et lorem id felis nonummy placerat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nullam sit amet magna in magna gravida vehicula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quis nibh at felis congue commodo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        <p id="cssContent">Přečtěte si příběhy těch, kterým prevence zachránila život. A že to někdy bylo opravdu takříkajíc "za 5 12". TBD Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer pellentesque quam vel velit. Praesent vitae arcu tempor neque lacinia pretium. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Aliquam in lorem sit amet leo accumsan lacinia. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nunc tincidunt ante vitae massa. Nam quis nulla. Phasellus et lorem id felis nonummy placerat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nullam sit amet magna in magna gravida vehicula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quis nibh at felis congue commodo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
         <Slider {...settings}>
           {data.map ( item => (
             <Col>
