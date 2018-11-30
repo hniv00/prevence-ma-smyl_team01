22923932cf66497d9d51f62116850325a969a536
David Červenka <cerd04@vse.cz>
2018-11-30 17:32:13 +0100

    Maximum length of text on cards is 6 lines

--- frontend/src/components/organisms/Stories.js
+++ frontend/src/components/organisms/Stories.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Card, CardTitle, CardText, Row, Col } from 'reactstrap';
 import {StoryButton} from '../atoms/StoryButton';
 
+import TextEllipsis from 'react-text-ellipsis';
 import Slider from "react-slick";
 
 const data = [{
@@ -15,33 +16,41 @@ const data = [{
     id: 2,
     name: 'Daniela',
     age: '23',
-    description: `Po procházce v lese jsem se pořádně neprohlédl.
-    O několik dní později jsem si na těle našel zakouslé klíště, okolo
-    místa vkusu zarudlé místo. Ihned jsem jel k lékaři, ale nepomohlo to.`
+    description: `Tak dnes dorazilo!!! Mile ráda podpořím nadační fond
+    @onemanshowfoundation !!! #1z10 protože je to to nejmenší co mohu udělat!!!
+    A jsem ráda, že kolegové z branže se podíleli na skvělým singlu Cizí zeď
+    #cizized a @kazma_kazmitch ať si myslí každý kdo chce, co chci..
+    Kamil má prostě skvělý nápady!!!... `
 }, {
     id: 3,
     name: 'Jiří',
     age: '53',
-    description: `Po procházce v lese jsem se pořádně neprohlédl.
-    O několik dní později jsem si na těle našel zakouslé klíště, okolo
-    místa vkusu zarudlé místo. Ihned jsem jel k lékaři, ale nepomohlo to.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit.`
+    description: `Dnes 26.9. krasne pocasi na D5. Unava nebo nepozornost
+    a prede mnou v pravem pruhu kamion a ridic, ktery nebrzdi pred kolonou,
+    ktera stoji v pravem jiznim pruhu. Celni naraz v plne rychlosti do
+    posledniho cisternoveho kamionu v kolone. Obrovska rana odhozeny kamion
+    a bily oblak z bileho prasku, ktery prevazela cisterna. Na miste ihned
+    mnoho lidi, ale vytekajici nafta z nadrze vetsinu lidi odradila od pomoci
+    zaklinenemu ridici. Na fotce ti, kteri to nevzdali.
+    Cesko-Slovensko-Nemecka pomoc nez prijely zachranne slozky.`
 }, {
     id: 4,
     name: 'Dominika',
     age: '63',
-    description: `Po procházce v lese jsem se pořádně neprohlédl.
-    O několik dní později jsem si na těle našel zakouslé klíště, okolo
-    místa vkusu zarudlé místo. Ihned jsem jel k lékaři, ale nepomohlo to.`
+    description: `Ten pocit, když je účastník kurzu na začátku rozechvělý
+    a říká, že si myslí, že to nemůže zvládnout. Pak mu nestačí základní
+    kurz a přijde i na ten pokročilý, kde na jeho konci musí fungovat při
+    autonehodě. Úplně v klidu zhodnotí situaci, neunikne mu, že člověk,
+    co byl nejdřív celkem v pohodě najednou faaakt nedýchá. Zvládne zavolat
+    na záchranku... s přehledem vytahuje zraněného z auta a zahájí
+    resuscitaci.... A co víc... Nezapomene na rukavice, které na začátku
+    považuje za zvláštní... To je prostě mazec v jednom dni. Úžasná proměna...`
 }, {
     id: 5,
     name: 'Roman',
     age: '20',
-    description: `Po procházce v lese jsem se pořádně neprohlédl.
-    O několik dní později jsem si na těle našel zakouslé klíště, okolo
-    místa vkusu zarudlé místo. Ihned jsem jel k lékaři, ale nepomohlo to.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit.`
+    description: `Každý by měl zastavit, nikdy nevíš kdy budeš
+    potřebovat pomoct ty!! Já zastavila jsem #1z10 #zastaviljsem`
 }]
 
 /* Custom arrows */
@@ -115,7 +124,17 @@ export class Stories extends Component {
               <Card id="cssCardStories" body>
                 <CardTitle>{ item.name }, { item.age }</CardTitle>
                 <CardText>
-                    <p id="cssStoriesParagraph">{item.description}</p>
+                    <p id="cssStoriesParagraph">
+                      <TextEllipsis
+                        lines={6}
+                        tag={'p'}
+                        ellipsisChars={' ...'}
+                        tagClass={'className'}
+                        debounceTimeoutOnResize={200}
+                        useJsOnly={true}>
+                        { item.description }
+                      </TextEllipsis>
+                    </p>
                 </CardText>
                 <div id="storyButton">
                   <a href="/pribehy"><StoryButton/></a>
