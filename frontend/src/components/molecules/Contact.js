35f6cad3bb442dd95799ccbc40a087c58ab5fe85
Veronika Hnilickova <hniv00@vse.cz>
2018-12-04 22:55:38 +0100

    some css

--- frontend/src/components/molecules/Contact.js
+++ frontend/src/components/molecules/Contact.js
@@ -9,18 +9,18 @@ export class Contact extends Component {
       <Jumbotron id="contactJumbo">
         <Row>
           <Col>
-            <h6>Naše sídlo</h6>
+            <h6 id="contactHeading">Naše sídlo</h6>
             <p>Pražská vinárna<br/>
             Staroměstské nám. 25<br/>
             110 00 Praha 1</p>
-            <h6>Telefon</h6>
+            <h6 id="contactHeading">Telefon</h6>
             <p>+420 600 723 860</p>
           </Col>
           <Col>
-            <h6>Fakturační údaje</h6>
+            <h6 id="contactHeading">Fakturační údaje</h6>
             <p>IČO: 02905639<br/>
             DIČ: CZ02905639</p>
-            <h6>Bankovní spojení</h6>
+            <h6 id="contactHeading">Bankovní spojení</h6>
             <p>7683180237/3030</p>
           </Col>
         </Row>
