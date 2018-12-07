7c0f7cc0abfb06405dcb345fab14de2d22d5672e
Daniel Navratil <navd00@vse.cz>
2018-12-07 18:33:40 +0100

    update pribehu
    update diagnoz
    list diagnoz

--- backend/src/modules/diagnosis/diagnosisRoutes.js
+++ backend/src/modules/diagnosis/diagnosisRoutes.js
@@ -4,10 +4,14 @@ import { Router } from 'express';
 import {
     newDiagController,
     deleteDiagController,
+    diagController,
+    updateDiagController,
 } from './diagnosisController';
 
 const router = expressAsyncAwait(Router());
 router.post('/newdiag/', newDiagController);
 router.post('/deletediag/:id', deleteDiagController);
+router.post('/updatediag/:id', updateDiagController);
+router.get('/list/', diagController);
 
 export default router;
--- backend/src/modules/stories/storyRouter.js
+++ backend/src/modules/stories/storyRouter.js
@@ -5,11 +5,13 @@ import {
     storyController,
     newStoryController,
     deleteStoryController,
+    updateStoryController,
 } from './storyController';
 
 const router = expressAsyncAwait(Router());
 router.get('/list/', storyController);
 router.post('/newstory/', newStoryController);
 router.post('/deletestory/:id', deleteStoryController);
+router.post('/updatestory/:id', updateStoryController);
 
 export default router;
