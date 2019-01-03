df10dc32e5335142fc6c46a7885b1f1f6e46a98b
Veronika Hnilickova <hniv00@vse.cz>
2019-01-03 23:40:54 +0100

    some details

--- frontend/src/components/atoms/ExaminationDetail.js
+++ frontend/src/components/atoms/ExaminationDetail.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { Modal, Row, Col, Button, ModalHeader, ModalBody, Badge } from 'reactstrap';
+import { Modal, Row, Col, Button, ModalHeader, ModalBody, Badge, Collapse } from 'reactstrap';
+import { BadgesDetail } from './BadgesDetail';
 
 export class ExaminationDetail extends Component {
   constructor(props) {
@@ -29,29 +30,34 @@ export class ExaminationDetail extends Component {
           <ModalHeader id="cssCardExamination" toggle={this.toggle} close={closeBtn}>{this.props.title}</ModalHeader>
           <ModalBody id="cssCardExamination">
             <p>{this.props.descr}</p>
-            <p>Díky preventivní prohlídce vyloučíte tyto diagnózy:</p>
-            <Row>
-              <Col style={{marginBottom: '20px'}}>
-                {this.props.diags.map((diag, i) => (
-                  <span>
-                <Badge id="cssBadge" color="secondary"
-                key={i} examination={diag.title}>
-                {diag.Name}
-                </Badge>{' '}
-                </span>
-              ))}
-              </Col>
-            </Row>
+
             <Row>
               <Col xs="12" sm="6">
-                <p>Základní periodicita pro toto vyšetření je:</p>
+                <p style={{marginBottom: '4px'}}>Základní periodicita pro toto vyšetření je:</p>
                 <p><Badge id="cssBadgePeriodicity">
                 {this.props.basicPeriodicity}</Badge>{' '}</p>
               </Col>
               <Col xs="12" sm="6">
-                <p>Ovšem dbejte zvýšenou pozornost v případech:</p>
+                <p style={{marginBottom: '4px'}}>Ovšem dbejte zvýšenou pozornost v případech:</p>
                 <p><Badge id="cssBadgePeriodicity">
                 {this.props.extPeriodicity}</Badge>{' '}</p>
+              </Col>
+            </Row>
+
+            <p style={{marginBottom: '4px'}}>Díky preventivní prohlídce vyloučíte tyto diagnózy:</p>
+            <Row>
+              <Col style={{marginBottom: '20px'}}>
+                  {this.props.diags.map((diag, i) => (
+                    <BadgesDetail style={{ 'cursor': 'pointer', 'marginRight': '5px' }} key={i} examination={diag.title} badge={diag.Name}>
+                      {diag.Name}
+                    </BadgesDetail>
+                  )
+                  )}
+                  <Collapse isOpen={this.state.toggle}>
+                    {this.props.diags.map((diag, i) => (
+                      <BadgesDetail style={{ 'cursor': 'pointer' }} key={i} examination={diag.title} badge={diag.Name} />)
+                    )}
+                  </Collapse>
               </Col>
             </Row>
           </ModalBody>
--- frontend/src/components/molecules/Examination.js
+++ frontend/src/components/molecules/Examination.js
@@ -21,25 +21,26 @@ export class Examination extends Component {
           <CardTitle>{this.props.data.ExamName}</CardTitle>
           <CardText>
             <Row>
-              <Col style={{ 'text-align': 'left' }}>
+              <Col style={{ 'textAlign': 'left' }}>
                 <CardText>
                   <Row>
                     {this.props.data.Diagnosis.slice(1, 4).map((diag, i) => (
-                      <BadgesDetail style={{ 'margin-right': '5px', 'cursor': 'pointer' }} key={i} examination={this.props.data.title} badge={diag.Name} />)
+                      <BadgesDetail style={{ 'marginRight': '5px', 'cursor': 'pointer' }} key={i} examination={this.props.data.title} badge={diag.Name} />)
                     )}
                     <Collapse isOpen={this.state.toggle}>
                       {this.props.data.Diagnosis.slice(4, this.props.data.Diagnosis.length).map((diag, i) => (
-                        <BadgesDetail style={{ 'margin-right': '5px', 'cursor': 'pointer' }} key={i} examination={this.props.data.title} badge={diag.Name} />)
+                        <BadgesDetail style={{ 'marginRight': '5px', 'cursor': 'pointer' }} key={i} examination={this.props.data.title} badge={diag.Name} />)
                       )}
                     </Collapse>
                     {this.props.data.Diagnosis.length - 3 > 0 &&
-                      <i class="material-icons" style={{ color: "#F26D98", 'margin-left': '6px' }} onClick={this.toggleCollapse}>more_horiz</i>}
+                      <i class="material-icons" style={{ color: "#F26D98", 'marginLeft': '6px', 'cursor': 'pointer' }} onClick={this.toggleCollapse}>more_horiz</i>}
                   </Row>
                 </CardText>
               </Col>
               <Col>
-                <p style={{ 'text-align': 'right' }}><Badge id="cssBadgePeriodicity">{this.props.data.Periodicity_ext}</Badge>{' '}
-                <i class="material-icons" style={{ color: "#39B2C3", 'margin-left': '6px' }}>more_horiz</i>
+                <p style={{ 'textAlign': 'right'}}>
+                <Badge id="cssBadgePeriodicity" style={{'marginBottom': '4px' }}>{this.props.data.Periodicity_ext}</Badge>{' '}
+                { /* <i class="material-icons" style={{ color: "#39B2C3", 'marginLeft': '6px' }}>more_horiz</i> */ }
                 </p>
               </Col>
             </Row>
